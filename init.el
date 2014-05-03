c657fa69b7a71d0385c92f0acf3d99bc61cd8d17
Mark Williams <mjwalfredo@unknown60c5478b2148.gateway.2wire.net>
2014-05-03 14:20:14 -0400

    Added melpa packages Change theme to birds of paradise Ditched org-mode crap Always show line numbers

--- init.el
+++ init.el
@@ -1,13 +1,9 @@
 (add-to-list 'load-path "~/.emacs.d")
-(add-to-list 'load-path "~/.emacs.d/color-theme-6.6.0")
-(add-to-list 'load-path "~/.emacs.d/emacs-colors-solarized")
+(add-to-list 'load-path "~/Library/Application Support/Aquamacs Emacs/elpa/birds-of-paradise-plus-theme-20130419.1429")
+(add-to-list 'custom-theme-load-path "~/Library/Application Support/Aquamacs Emacs/elpa/birds-of-paradise-plus-theme-20130419.1429")
+(require 'birds-of-paradise-plus-theme)
 
-
-(require 'color-theme)
-(require 'color-theme-solarized)
-
-(setq org-mobile-directory "~/Dropbox/MobileOrg")
-
+;; Set window size just right
 (setq default-frame-alist '(
                 (width . 160)
                 (height . 49) ))
@@ -23,25 +19,8 @@
  '(gud-gdb-command-name "gdb --annotate=1")
  '(inhibit-startup-screen t)
  '(large-file-warning-threshold nil)
- '(org-agenda-custom-commands (quote (("d" todo "DELEGATED" nil) ("c" todo "DONE|DEFERRED|CANCELLED" nil) ("w" todo "WAITING" nil) ("W" agenda "" ((org-agenda-ndays 21))) ("A" agenda "" ((org-agenda-skip-function (lambda nil (org-agenda-skip-entry-if (quote notregexp) "\\=.*\\[#A\\]"))) (org-agenda-ndays 1) (org-agenda-overriding-header "Today's Priority #A tasks: "))) ("u" alltodo "" ((org-agenda-skip-function (lambda nil (org-agenda-skip-entry-if (quote scheduled) (quote deadline) (quote regexp) "
-]+>"))) (org-agenda-overriding-header "Unscheduled TODO entries: "))))))
- '(org-agenda-files (quote ("~/Dropbox/org/todo.org" "~/Dropbox/org/notes.org")))
- '(org-agenda-ndays 7)
- '(org-agenda-show-all-dates t)
- '(org-agenda-skip-deadline-if-done t)
- '(org-agenda-skip-scheduled-if-done t)
- '(org-agenda-start-on-weekday nil)
- '(org-deadline-warning-days 14)
- '(org-default-notes-file "~/Dropbox/org/notes.org")
- '(org-fast-tag-selection-single-key (quote expert))
- '(org-mobile-files (quote (org-agenda-files "~/Dropbox/org/notes.org" "~/Dropbox/org/todo.org_archive")))
- '(org-remember-store-without-prompt t)
- '(org-remember-templates (quote ((116 "* TODO %?
-  %u" "~/Dropbox/org/todo.org" "Tasks") (110 "* %u %?" "~/Dropbox/org/notes.org" "Notes"))))
- '(org-reverse-note-order t)
- '(remember-annotation-functions (quote (org-remember-annotation)))
- '(remember-handler-functions (quote (org-remember-handler)))
  '(transient-mark-mode t))
+
 (custom-set-faces
   ;; custom-set-faces was added by Custom.
   ;; If you edit it by hand, you could mess it up, so be careful.
@@ -55,44 +34,8 @@
 ;; create the autosave dir if necessary, since emacs won't.
 (make-directory "~/.emacs.d/autosaves/" t)
 
-(require 'org-install)
-
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
-
-(define-key mode-specific-map [?a] 'org-agenda)
-
-(eval-after-load "org-agenda"
-  '(progn
-     (define-prefix-command 'org-todo-state-map)
-
-     (define-key org-mode-map "\C-cx" 'org-todo-state-map)
-
-     (define-key org-todo-state-map "x"
-       #'(lambda nil (interactive) (org-todo "CANCELLED")))
-     (define-key org-todo-state-map "d"
-       #'(lambda nil (interactive) (org-todo "DONE")))
-     (define-key org-todo-state-map "f"
-       #'(lambda nil (interactive) (org-todo "DEFERRED")))
-     (define-key org-todo-state-map "l"
-       #'(lambda nil (interactive) (org-todo "DELEGATED")))
-     (define-key org-todo-state-map "s"
-       #'(lambda nil (interactive) (org-todo "STARTED")))
-     (define-key org-todo-state-map "w"
-       #'(lambda nil (interactive) (org-todo "WAITING")))
-
-     (define-key org-agenda-mode-map "\C-n" 'next-line)
-     (define-key org-agenda-keymap "\C-n" 'next-line)
-     (define-key org-agenda-mode-map "\C-p" 'previous-line)
-     (define-key org-agenda-keymap "\C-p" 'previous-line)))
-
-(require 'remember)
-
-(add-hook 'remember-mode-hook 'org-remember-apply-template)
-
-(define-key global-map [(control meta ?r)] 'remember)
-
-;;; turn on syntax highlighting
-(global-font-lock-mode 1)
+;;; turn off syntax highlighting?
+(global-font-lock-mode nil)
 
 ;;; use groovy-mode when file ends in .groovy or has #!/bin/groovy at start
 (autoload 'groovy-mode "groovy-mode" "Major mode for editing Groovy code." t)
@@ -105,6 +48,13 @@
              (require 'groovy-electric)
              (groovy-electric-mode)))
 
+;;; melpa packages for Emacs coolness
+(when (>= emacs-major-version 24)
+  (require 'package)
+  (package-initialize)
+  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+  )
 
+;;; Always display line numbers in left margin
+(global-nlinum-mode 1)
 
-(color-theme-solarized-light)
